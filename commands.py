1ca95832110ea89679329d97fbe1e7eae58d2000
Shikatsu Kagaminara <misterarkandias@gmail.com>
2018-08-08 09:41:15 +0200

    Command class refactor, introduction of subcommands

--- commands.py
+++ commands.py
@@ -1,12 +1,3 @@
-class Command:
-    __slots__ = (
-        'helper',
-        'trigger',
-        'subCommand'
-    )
-    def __call__(self):
-        raise NotImplementedError()
-
 class CommandsRegistry:
     def __init__(self):
         self.commands = {}
@@ -16,13 +7,28 @@ class CommandsRegistry:
     def get(self, command):
         return self.commands.get(command, HelpCommand())
 
+class Command:
+    __slots__ = (
+        'helper',
+        'trigger',
+    )
+    subcommands = CommandsRegistry()
+
+    def __call__(self, message):
+        try:
+            return self.handler(message)
+        except NotImplementedError:
+            return self.subcommands.get(message[0])(message[1:])
+
+    def handler(self):
+        raise NotImplementedError()
+
 commands = CommandsRegistry()
 
 @commands.register
 class SuccessCommand(Command):
     helper = 'Try something.'
     trigger = 'success'
-    subCommand = CommandsRegistry()
     def __call__(event):
         return 'You successfully failed'
 
@@ -30,7 +36,6 @@ class SuccessCommand(Command):
 class FailCommand(Command):
     helper = 'Try something.'
     trigger = 'fail'
-    subCommand = CommandsRegistry()
     def __call__(event, message):
         return 'You tried something. You failed'
 
@@ -38,7 +43,6 @@ class FailCommand(Command):
 class TravelCommand(Command):
     helper = 'Try to move a bit .'
     trigger = 'travel'
-    subCommand = CommandsRegistry()
     def __call__(event, message):
         return 'You travel a bit to try to change your peevish, poor life. You failed.'
 
@@ -46,7 +50,6 @@ class TravelCommand(Command):
 class DungeonCommand(Command):
     helper = 'Try to explore a dungeon.'
     trigger = 'dungeon'
-    subCommand = CommandsRegistry()
     def __call__(event, message):
         return 'A latex-covered dominatrix comes to you with a leather whip. You undestand that\'s not the kind of dungeon you want to explore.'
 
@@ -54,11 +57,10 @@ class DungeonCommand(Command):
 class HelpCommand(Command):
     helper = 'Show this help.'
     trigger = 'help'
-    subCommand = CommandsRegistry()
     def __call__(event, message):
         ret = "```"
         for commandKey, command in commands.commands.items():
             ret += '{} - {}\n'.format(command.trigger, command.helper)
-            for subCommandKey, subCommand in command.subCommand.commands.items():
+            for subCommandKey, subCommand in command.subcommands.commands.items():
                 ret += '\t{} - {}\n'.format(subCommand.trigger, subCommand.helper)
         return ret + '```'
